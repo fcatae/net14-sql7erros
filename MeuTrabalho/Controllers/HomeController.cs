63f778d7270251ca360a4888daeff3affd4c8d08
Fabricio Catae <fcatae@microsoft.com>
2019-01-31 10:02:02 -0200

    add home controller

--- MeuTrabalho/Controllers/HomeController.cs
+++ MeuTrabalho/Controllers/HomeController.cs
@@ -5,11 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MeuTrabalho.Models;
+using System.Data.SqlClient;
 
 namespace MeuTrabalho.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : Controller, IDisposable
     {
+        SqlConnection connection;
+
+        public HomeController()
+        {
+            this.connection = new SqlConnection("Server=saturnoserver.database.windows.net;Database=MEUDB;User=app;Password=homework-jan31;Max Pool Size=10");
+        }
+
         public IActionResult Index()
         {
             return RedirectToActionPermanent("Index", "Account");
@@ -24,12 +32,38 @@ namespace MeuTrabalho.Controllers
         {
             ViewData["Message"] = "Your application description page.";
 
+            try
+            {
+                this.connection.Open();
+
+                SqlCommand sql = new SqlCommand("INSERT tbLog VALUES ('about')", this.connection);
+                sql.ExecuteReader();                
+            }
+            catch(Exception ex)
+            {
+                ViewData["Message"] = "ERROR ABOUT";
+            }
+
             return View();
         }
 
         public IActionResult Contact()
         {
             ViewData["Message"] = "Your contact page.";
+
+            try
+            {
+                SqlConnection conn1 = this.connection;
+
+                SqlCommand sql = new SqlCommand("INSERT tbLog VALUES ('contact')");
+                sql.Connection = conn1;
+
+                sql.ExecuteScalar();
+            }
+            catch(Exception ex)
+            {
+                return RedirectToAction("Error");
+            }
 
             return View();
         }
